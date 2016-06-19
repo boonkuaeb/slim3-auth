79d7ed6fb16043d3f1b860dd1efca1a98a300436
boonkuae_boo <john.kuae@gmail.com>
2016-06-19 12:30:14 +0700

    10-Signing the user up

--- app/Controllers/Auth/AuthController.php
+++ app/Controllers/Auth/AuthController.php
@@ -3,6 +3,7 @@
 namespace Slim3Auth\Controllers\Auth;
 
 use Slim3Auth\Controllers\Controller;
+use Slim3Auth\Models\User;
 
 class AuthController extends Controller
 {
@@ -14,6 +15,16 @@ class AuthController extends Controller
 
     public function postSignUp($request, $response)
     {
+
+        User::create(
+            [
+                'email' => $request->getParam('email'),
+                'name' => $request->getParam('name'),
+                'password' => password_hash( $request->getParam('password'),PASSWORD_DEFAULT)
+            ]
+        );
+
+        return $response->withRedirect($this->router->pathFor('home'));
 
     }
 }
