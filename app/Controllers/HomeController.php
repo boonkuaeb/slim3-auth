aedbe1c31479397cf288a7845704cefc2a4d94cd
boonkuae_boo <john.kuae@gmail.com>
2016-06-19 11:46:30 +0700

    sample get data from db

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -10,6 +10,28 @@ class HomeController extends Controller
 
     public function index($request, $response)
     {
+
+        try{
+            echo '<pre>';
+
+
+            $user = $this->db->table('users')->where('id','=',1)->get();
+            print_r($user);
+            echo '<hr/>';
+
+            $user =  $this->db->table('users')->find(1);
+            print_r($user);
+            echo '<hr/>';
+
+
+        }catch(\Exception $e)
+        {
+            print_r($e->getMessage());
+        }
+
+
+
+        die;
         $this->view->render($response,"home.twig");
     }
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,12 +6,34 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $app = new \Slim\App([
     'settings' => [
-        'displayErrorDetails' => true
+        'displayErrorDetails' => true,
+        'determineRouteBeforeAppMiddleware' => true,
+        'addContentLengthHeader' => false,
+        'db'=> [
+            'driver' => 'mysql',
+            'host' => 'laradock_mysql_1',
+            'database' => 'slim3_auth_db',
+            'username' => 'root',
+            'password' => 'root',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => ''
+        ]
     ]
 ]);
 
 
 $container = $app->getContainer();
+$capsule = new Illuminate\Database\Capsule\Manager;
+$capsule->addConnection($container['settings']['db']);
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
+
+$container['db'] = function($container) use ($capsule)
+{
+    return $capsule;
+};
+
 $container['view'] = function ($container) {
     $view = new \Slim\Views\Twig(__DIR__ . '/../resources/views', [
         'cache' => false // production set true
